f7b9595a1e1c4b206ef32428c71b5c311a413f4f
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-30 23:32:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  counter = 0 
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find{|type| type == cheese_types.any}
   
